47f34440b6aeef5ec1180aa51c2eab4d761d549f
Alexandre Thorez <alexandre.thorez@etudiant.univ-lille1.fr>
2017-02-09 12:18:29 +0100

    generate group entity

---
+++ src/AppBundle/Entity/Group.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thoreza
+ * Date: 09/02/17
+ * Time: 12:10
+ */
+
+namespace AppBundle\Entity;
+
+
+namespace AppBundle\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Group
+{
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+
+    /**
+     * @param mixed $users
+     */
+    public function setUsers($users)
+    {
+        $this->users = $users;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdmin()
+    {
+        return $this->admin;
+    }
+
+    /**
+     * @param mixed $admin
+     */
+    public function setAdmin($admin)
+    {
+        $this->admin = $admin;
+    }
+    /**
+     * @var string The group identifier
+     *
+     * @ORM\Column(type="guid")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="UUID")
+     */
+    private $id;
+
+    /**
+     * @var string The group name
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank
+     */
+    private $name;
+
+    /**
+     * @var string The group city
+     *
+     * @ORM\Column(type="string")
+     */
+    private $city;
+
+    /**
+     * @var string The group description
+     *
+     * @ORM\Column(type="text")
+     */
+    private $description;
+    private $users;
+    private $admin;
+}
